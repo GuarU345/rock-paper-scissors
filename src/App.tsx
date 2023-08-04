bf31600ce110cc2aa036049a7e20d5779812992b
abel34 <abel.dominguez@sw-argos.com>
2023-08-04 09:32:00 -0600

    install react-icons and create the design of home

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import "./App.css";
+import { FaHandRock, FaHandScissors } from "react-icons/fa";
+import { LiaToiletPaperSolid } from "react-icons/lia";
 
 function App() {
   return (
     <>
-      <h1 className="text-6xl">Rock Paper Scissors</h1>
+      <h1 className="text-2xl text-center text-white">Rock Paper Scissors</h1>
+      <main>
+        <ul className="flex flex-col items-center pt-6 text-white gap-3">
+          <li className="grid place-content-center border-2 w-[200px] h-[200px] text-6xl">
+            <FaHandRock />
+          </li>
+          <li className="grid place-content-center border-2 w-[200px] h-[200px] text-6xl">
+            <LiaToiletPaperSolid />
+          </li>
+          <li className="grid place-content-center border-2 w-[200px] h-[200px] text-6xl">
+            <FaHandScissors />
+          </li>
+        </ul>
+      </main>
     </>
   );
 }
