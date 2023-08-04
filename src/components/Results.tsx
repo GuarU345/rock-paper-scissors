b17ca463bb117a1bb7a6ae870cf7058f896da9a8
abel34 <abel.dominguez@sw-argos.com>
2023-08-04 13:00:03 -0600

    create component results to show the final results of the game

---
+++ src/components/Results.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from "react";
+
+type Props = {
+  user: ReactNode;
+  cpu: ReactNode;
+};
+
+const Results = ({ user, cpu }: Props) => {
+  return (
+    <div className="flex flex-col text-white items-center gap-2">
+      <section>
+        <p className="text-center">Tu</p>
+        <span className="text-6xl">{user}</span>
+      </section>
+
+      <section>
+        <p className="text-center">Computadora</p>
+        <span className="text-6xl [&>svg]:m-auto">{cpu}</span>
+      </section>
+    </div>
+  );
+};
+
+export default Results;
