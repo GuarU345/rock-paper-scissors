809f7c0288084e4c1e661c2b628004fd38604784
abel34 <abel.dominguez@sw-argos.com>
2023-08-04 13:12:40 -0600

    fix dont leave in a cellphone

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -19,7 +19,10 @@ const Modal = ({ children, text, handleClose }: PropsWithChildren<Props>) => {
   }, []);
   return (
     <>
-      <div className="absolute flex flex-col top-0 left-0 h-full w-full bg-black bg-opacity-50">
+      <div
+        className="absolute flex flex-col top-0 left-0 h-full w-full bg-black bg-opacity-50"
+        onClick={handleClose}
+      >
         <div className="absolute border-2 top-1/2 left-1/2 transform w-4/6 -translate-x-1/2 -translate-y-1/2 bg-black shadow-2xl z-10 h-2/6 rounded-lg xl:w-2/6 xl:h-60">
           <section className="flex flex-col items-center justify-center">
             <h1 className="text-xl text-white">{text}</h1>
