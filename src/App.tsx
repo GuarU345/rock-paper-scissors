53662cc1cad0ae60700d9657cc74bd68a6787747
guaru345 <abelmtz34@outlook.com>
2023-08-10 10:16:52 -0600

    add home component and install react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,17 @@
-import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { FaHandRock, FaHandScissors } from "react-icons/fa";
-import { LiaToiletPaperSolid } from "react-icons/lia";
-import { OPTIONS, WINNER_OPTIONS } from "./constants.tsx";
-import Modal from "./components/Modal";
-import Options from "./components/Options.tsx";
-import confetti from "canvas-confetti";
-import Results from "./components/Results.tsx";
-import { generateOptionOfCpu } from "./functions/random.ts";
+import Home from "./pages/Home";
+import Game from "./pages/Game";
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [text, setText] = useState("");
-  const [userOption, setUserOption] = useState<JSX.Element | undefined>();
-  const [cpuOption, setCpuOption] = useState<JSX.Element | undefined>();
-
-  const closeModal = () => {
-    setIsOpen(false);
-  };
-
-  const game = (option: number, element: JSX.Element) => {
-    const cpuOption = generateOptionOfCpu();
-    if (option === cpuOption.value) {
-      setIsOpen(true);
-      setText("Draw");
-      setUserOption(element);
-      setCpuOption(cpuOption.element);
-      return;
-    }
-    if (WINNER_OPTIONS[option] == cpuOption.value) {
-      setIsOpen(true);
-      confetti();
-      setText("You Win");
-      setUserOption(element);
-      setCpuOption(cpuOption.element);
-      return;
-    }
-    setIsOpen(true);
-    setText("You Lost");
-    setUserOption(element);
-    setCpuOption(cpuOption.element);
-    return;
-  };
-
   return (
     <>
-      <h1 className="text-2xl text-center text-white">Rock Paper Scissors</h1>
-      <main className="grid place-content-center h-full pb-4 overflow-hidden">
-        <ul className="flex flex-col items-center pt-6 text-white gap-3 md:flex-row md:justify-center">
-          <Options
-            action={() => game(OPTIONS.rock.value, OPTIONS.rock.element)}
-            icon={<FaHandRock />}
-          />
-          <Options
-            action={() => game(OPTIONS.paper.value, OPTIONS.paper.element)}
-            icon={<LiaToiletPaperSolid />}
-          />
-          <Options
-            action={() =>
-              game(OPTIONS.scissors.value, OPTIONS.scissors.element)
-            }
-            icon={<FaHandScissors />}
-          />
-        </ul>
-      </main>
-      {isOpen ? (
-        <div>
-          <Modal text={text} handleClose={closeModal}>
-            <Results user={userOption} cpu={cpuOption} action={closeModal} />
-          </Modal>
-        </div>
-      ) : null}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/game" element={<Game />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useRef } from "react";
+import { BsScissors } from "react-icons/bs";
+import { GiStoneBlock } from "react-icons/gi";
+import { IoIosPaper } from "react-icons/io";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  const elementRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (elementRef.current) {
+      const elementsToAppear =
+        elementRef.current?.querySelectorAll(".animate-appear");
+
+      elementsToAppear?.forEach((element) => {
+        element.classList.remove("opacity-0");
+      });
+    }
+  }, []);
+
+  return (
+    <div
+      className="grid place-content-center h-screen text-center text-white text-6xl"
+      ref={elementRef}
+    >
+      <h1 className="font-bold flex justify-center mb-4 opacity-0 transition-opacity duration-1000 delay-900 ease-in animate-appear xl:text-8xl">
+        <span className="text-gray-400">
+          <GiStoneBlock />
+        </span>
+        Rock
+      </h1>
+      <h1 className="font-bold flex justify-center mb-4 opacity-0 transition-opacity duration-1000 delay-500 ease-in animate-appear xl:text-8xl">
+        Paper
+        <span>
+          <IoIosPaper className="text-yellow-400" />
+        </span>
+      </h1>
+      <h1 className="font-bold flex justify-center mb-4 opacity-0 transition-opacity duration-1000 delay-700 ease-in animate-appear xl:text-8xl">
+        Sciss
+        <span>
+          <BsScissors className="text-red-600" />
+        </span>
+        ors
+      </h1>
+      <Link
+        type="button"
+        to="/game"
+        className="border-2 p-2 text-base mt-4 rounded-md hover:border-sky-600"
+      >
+        PLAY GAME
+      </Link>
+    </div>
+  );
+};
+
+export default Home;
