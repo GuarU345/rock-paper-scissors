a9d427a8facf99ff010fa519fdd08f77d8b11eed
guaru345 <abelmtz34@outlook.com>
2023-08-10 15:37:23 -0600

    add const api url and new function to get the options of the game

--- src/services/game.ts
+++ src/services/game.ts
@@ -1,21 +1,28 @@
 import axios from "axios";
-import { Room, Response } from "../types";
+import { Room, Response, Game, GameBody } from "../types";
+
+const API_URL = "http://127.0.0.1:8090/api/collections";
 
 const createRoom = async (body: string) => {
-  const { data } = await axios.post<Room>(
-    "http://127.0.0.1:8090/api/collections/rooms/records",
-    {
-      name: body,
-    }
-  );
+  const { data } = await axios.post<Room>(`${API_URL}/rooms/records`, {
+    name: body,
+  });
+  return data;
+};
+
+const newGame = async (body: GameBody) => {
+  const { data } = await axios.post<Game>(`${API_URL}/game/records`, body);
   return data;
 };
 
 const getDisponibleRooms = async () => {
-  const { data } = await axios.get<Response<Room>>(
-    "http://127.0.0.1:8090/api/collections/rooms/records"
-  );
+  const { data } = await axios.get<Response<Room>>(`${API_URL}/rooms/records`);
   return data.items.filter((r) => r.playing === false);
 };
 
-export { createRoom, getDisponibleRooms };
+const getOptions = async () => {
+  const { data } = await axios.get(`${API_URL}/options/records`);
+  return data.items;
+};
+
+export { createRoom, getDisponibleRooms, newGame, getOptions };
