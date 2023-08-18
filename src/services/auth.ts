56b4d91e0f462a7a241b48d94e6bdfee3a54abfe
guaru345 <abelmtz34@outlook.com>
2023-08-18 13:27:19 -0600

    manage errors in login and register

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,16 +1,33 @@
-import axios from "axios";
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { SigninBody, SignupBody } from "../types";
+import PocketBase from "pocketbase";
 
-const API_URL = "https://rps-db.pockethost.io/api/collections";
+const pb = new PocketBase("https://rps-db.pockethost.io");
 
 const signup = async (body: SignupBody) => {
-  const { data } = await axios.post(`${API_URL}/users/records`, body);
-  return data;
+  try {
+    const { data } = await await pb.collection("users").create(body);
+    return data;
+  } catch (error: any) {
+    if (error.response.data.email) {
+      throw new Error(error.response.data.email.message);
+    }
+    if (error.response.data.passwordConfirm) {
+      throw new Error(error.response.data.passwordConfirm.message);
+    }
+    throw new Error(error);
+  }
 };
 
 const signin = async (body: SigninBody) => {
-  const {data} = await axios.post(`${API_URL}/users/auth-with-password`,body)
-  return data.token
-}
+  try {
+    const authData = await pb
+      .collection("users")
+      .authWithPassword(body.identity, body.password);
+    return authData.token;
+  } catch (error: any) {
+    throw new Error(error.response.message);
+  }
+};
 
 export { signup, signin };
