dc04651f89306455e8c53e924851dd6d617ce0a0
guarU345 <abelmtz34@outlook.com>
2024-02-20 19:38:14 -0600

    best design of puntuations

--- src/components/Puntuations.tsx
+++ src/components/Puntuations.tsx
@@ -5,12 +5,11 @@ type Props = {
 
 const Puntuations = ({ userPuntuation, cpuPuntuation }: Props) => {
   return (
-    <section className="flex text-xs text-center text-white gap-4 justify-center mb-5">
-      <h5 className="border-2 rounded-md p-2">
+    <section className="flex text-xs text-center text-white gap-8 justify-center mb-5">
+      <h5 className="border-2 rounded-md p-2 xl:w-32">
         Player: <span>{userPuntuation}</span>
       </h5>
-      <p></p>
-      <h5 className="border-2 rounded-md p-2">
+      <h5 className="border-2 rounded-md p-2 xl:w-32">
         Cpu: <span>{cpuPuntuation}</span>
       </h5>
     </section>
